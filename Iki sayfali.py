a9f2e3e1d31aca7f6627881eb8600040aa5df659
okayalp <okayalp@gmail.com>
2021-06-06 14:48:48 +0300

    iki sayfalı olarak çalışıyor

--- Iki sayfali.py
+++ Iki sayfali.py
@@ -1,38 +1,45 @@
+import time
+
 from kivy.animation import Animation
-from kivy.properties import StringProperty, NumericProperty
+from kivy.properties import NumericProperty
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 
+
 def pageone():
     class FirstPage(App):
         def build(self):
             self.window1 = GridLayout()
             self.window1.cols = 1
+            self.Header1 = Label(text="TIMER V. 0.1", font_size="70sp")
+            self.window1.add_widget(self.Header1)
+            self.message1 = Label(text="Input Timer Seconds")
+            self.window1.add_widget(self.message1)
+            self.entry1 = TextInput(multiline=False)
 
-            Header = Label(text="TIMER V. 0.1", font_size="70sp")
-            self.window1.add_widget(Header)
-
-            self.message = Label(text="Input Timer Seconds")
-            self.window1.add_widget(self.message)
-
-            self.entry = TextInput(multiline=False)
-            self.window1.add_widget(self.entry)
-
-            self.dugme = Button(text="Click here after input")
-            self.dugme.bind(on_press=self.callit)
-            self.window1.add_widget(self.dugme)
+            self.window1.add_widget(self.entry1)
+            self.dugme1 = Button(text="Click here after input")
+            self.dugme1.bind(on_press=self.callit)
+            self.window1.add_widget(self.dugme1)
             return self.window1
 
         def callit(self, event):
             try:
                 global a
-                a = (self.entry.text)
-                pagetwo()
+                a = (self.entry1.text)
+                a = int(a)
+                self.window1.remove_widget(self.Header1)
+                self.window1.remove_widget(self.message1)
+                self.window1.remove_widget(self.entry1)
+                self.window1.remove_widget(self.dugme1)
+                FirstPage().stop()
+                return pagetwo()
+
             except:
-                self.message.text = "THIS IS NOT A NUMBER " + self.entry.text + " !!!"
+                self.message1.text = "THIS IS NOT A NUMBER "
 
     if __name__ == "__main__":
         FirstPage().run()
@@ -59,33 +66,11 @@ def pagetwo():
         def build(self):
             cronometer = Cronoanimation()
             self.window2 = GridLayout(cols=1)
-
             Header = Label(text="TIMER V. 0.1", font_size="70sp")
             self.window2.add_widget(Header)
-
-            #self.MessageLine = Label(text="Input Time in secs")
-            #self.window2.add_widget(self.MessageLine)
-
-            #self.inputsec = TextInput(multiline=False)
-            #self.window2.add_widget(self.inputsec)
-
-            #Click = Button(text="Click")
-            #Click.bind(on_press=self.event)
-            #self.window2.add_widget(Click)
-
             self.window2.add_widget(cronometer)
             cronometer.start()
-
             return self.window2
-
-        def event(self, number):
-            try:
-                global secint
-                secint = int(self.inputsec.text)
-                self.MessageLine.text = "Count Down From : " + self.inputsec.text
-                print(type(secint))
-            except:
-                self.MessageLine.text = "Input a Number"
 
     if __name__ == "__main__":
         Timer().run()
